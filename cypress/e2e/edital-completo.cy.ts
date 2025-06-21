d25f1cd5424010afdca8d986ec2f4c6589d92c72
ViniciusC-A <viniciuscarneiro10@hotmail.com>
2025-06-21 00:58:28 -0400

    Subindo edital médio e simples, completo feito parcial

--- cypress/e2e/edital-completo.cy.ts
+++ cypress/e2e/edital-completo.cy.ts
@@ -36,24 +36,35 @@ describe('Sistema Integrado de Gestão para Fundações de Amparo a Pesquisas', 
     cy.get('[data-cy="abrangencia"]').click();
     cy.get('[data-cy="estado-todos"]').click();
 
-    //Informações Complementares
+    
+     //Informações Complementares
     cy.get('[data-cy="informacoes-complementares"]').click();
     cy.get('[data-cy="perguntaInfoId"]').click();
     cy.get('[data-cy="ocupacao-da-equi"]').click();
+    cy.wait(300);
     cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click(); 
 
     cy.get('[data-cy="perguntaInfoId"]').click();
     cy.get('[data-cy="objetivos-de-des"]').click();
+    cy.wait(300);
     cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click(); 
 
     cy.get('[data-cy="perguntaInfoId"]').click();
     cy.get('[data-cy="porte-da-empresa"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click();
+
+    cy.get('[data-cy="perguntaInfoId"]').click();
+    cy.get('[data-cy="areas-tematicas"]').click();
+    cy.wait(500);
+    cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click();
+
+    cy.get('[data-cy="perguntaInfoId"]').click();
+    cy.get('[data-cy="data-de-realizac"]').click();
+    cy.wait(300);
     cy.get('[data-cy="informacaoComplementarPergunta-adicionar"]').click();
     
-    
 
-
-    //
     cy.get('[data-cy="cronograma"]').click(); //Clica na aba Cronograma para seguir para a página de Cronograma
     cy.get('[data-cy="periodo-de-submissao"]').click(); //Clica na aba Período de Submissão para seguir para a página de Período de Submissão
     cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar um novo Período de Submissão
@@ -66,12 +77,196 @@ describe('Sistema Integrado de Gestão para Fundações de Amparo a Pesquisas', 
     cy.get('[data-cy="programa"]').click(); //Clica em Programa para seguir para a página de Programa
     cy.get('[data-cy="programaId"]').click(); //Clica no campo de seleção de Programa
     cy.get('[data-cy-index="programaId-item-2"]').click(); 
-    cy.get('[data-cy="add-natureza-da-despesa"]').click(); 
-    cy.get('[data-cy="naturezaDespesaEditalUnsaved.naturezaDespesaId"]').click();
-    cy.get('[data-cy="capital"]').click(); 
+    //cy.get('[data-cy="add-natureza-da-despesa"]').click(); 
+    //cy.get('[data-cy="naturezaDespesaEditalUnsaved.naturezaDespesaId"]').click();
+    //cy.get('[data-cy="capital"]').click(); 
+    //cy.get('[data-cy="naturezaDespesaEditalUnsaved.valor"]').type('10');
+    //cy.get('[data-cy="naturezaDespesaEdital-confirmar"]').click();
+
+    //RUBRICAS
     
-    cy.get('[data-cy="naturezaDespesaEditalUnsaved.valor"]').type('10');
-    cy.get('[data-cy="naturezaDespesaEdital-confirmar"]').click();
+    cy.get('[data-cy="rubricas"]').click(); 
+    cy.get('[data-cy="add-button"]').click(); 
+
+    //rubrica 1
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="diarias"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="capital"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+
+    //rubrica 2
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="hospedagem-e-ali"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="auxilio-a-pesqui"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+
+    //rubrica 3
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="servicos-de-terc"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="custeio"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+
+    //rubrica 4
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="material-de-cons"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="capital"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+
+    //rubrica 5
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="material-permane"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="capital"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+
+    //rubrica 6
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="passagens"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="auxilio-a-pesqui"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+
+    //rubrica 7
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="pessoal"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="auxilio-a-pesqui"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+
+    //rubrica 8
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="encargos"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="custeio"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+
+    //rubrica 9
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="editalRubricaUnsaved.tipoEditalRubrica"]').click();
+    cy.get('[data-cy="bolsa"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubricaUnsaved.naturezaDespesaId"]').click();
+    cy.get('[data-cy="auxilio-a-pesqui"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="editalRubrica-confirmar"]').click();
+    
+
+
+    //Faixas de Financiamento
+    //financiamento 1
+    cy.get('[data-cy="faixas-de-financiamento"]').click();
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 1'); 
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('1000');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('5000');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Observação da Faixa 1');
+    cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+    //financiamento 2
+    cy.get('[data-cy="faixas-de-financiamento"]').click();
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 2'); 
+    //cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('1050');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('50500');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Observação da Faixa 2');
+    cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+    //financiamento 3
+    cy.get('[data-cy="faixas-de-financiamento"]').click();
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 3'); 
+    //cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('1330');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('60000');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Observação da Faixa 3');
+    cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+    //financiamento 4
+    cy.get('[data-cy="faixas-de-financiamento"]').click();
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 4'); 
+    //cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('1330');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('70000');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Observação da Faixa 4');
+    cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+    //financiamento 5
+    cy.get('[data-cy="faixas-de-financiamento"]').click();
+    cy.get('[data-cy="add-button"]').click(); 
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.nome"]').type('Faixa 5'); 
+    //cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMinimo"]').type('1330');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.valorMaximo"]').type('80000');
+    cy.get('[data-cy="faixaFinanciamentoUnsaved.observacao"]').type('Observação da Faixa 5');
+    cy.get('[data-cy="faixaFinanciamento-confirmar"]').click();
+
+    //Descrição do Projeto
+
+    cy.get('[data-cy="perguntas"]').click(); 
+    cy.get('[data-cy="descricao-do-projeto"]').click(); 
+
+    //descriçao 1
+    cy.get('[data-cy="perguntaDescId"]').click();
+    cy.get('[data-cy="informacoes-rele"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="pergunta-adicionar"]').click();
+
+    //descriçao 2
+    cy.get('[data-cy="perguntaDescId"]').click();
+    cy.get('[data-cy="experiencia-do-c"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="pergunta-adicionar"]').click();
+  
+    //descriçao 3
+    cy.get('[data-cy="perguntaDescId"]').click();
+    cy.get('[data-cy="riscos-e-ativida"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="pergunta-adicionar"]').click();
+
+    //descriçao 4
+    cy.get('[data-cy="perguntaDescId"]').click();
+    cy.get('[data-cy="objetivo-geral"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="pergunta-adicionar"]').click();
+
+    //descriçao 5 
+    cy.get('[data-cy="perguntaDescId"]').click();
+    cy.get('[data-cy="referencia-bibli"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="pergunta-adicionar"]').click();
+
+    //descriçao 6
+    cy.get('[data-cy="perguntaDescId"]').click();
+    cy.get('[data-cy="metodos-e-materi"]').click();
+    cy.wait(300);
+    cy.get('[data-cy="pergunta-adicionar"]').click();
+    
+    
     cy.get('[data-cy="perguntas"]').click();
     cy.get('[data-cy="indicadores-de-producao"]').click();
 
